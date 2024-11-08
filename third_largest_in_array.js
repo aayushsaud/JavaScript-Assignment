7b3c82d53fe024a6cde8eabd698d7d86f3ab6694
aayushsaud <saudaayush.dev@gmail.com>
2024-11-08 22:30:23 +0545

    Third largest in array

---
+++ third_largest_in_array.js
@@ -0,0 +1,31 @@
+function thirdLargestInArray(array) {
+    // Array Size
+    let arraySize = array.length;
+
+    // Input Validation
+    if (arraySize < 3) {
+        console.log("Array must have at least 3 integers!");
+        return;
+    }
+
+     // Initializing first, second, and third largest integer to -Infinity
+     let first = -Infinity, second = -Infinity, third = -Infinity;
+
+     for (let i = 0; i < arraySize; i++) {
+         if (array[i] > first) {
+             third = second;
+             second = first;
+             first = array[i];
+         } else if (array[i] > second && array[i] < first) {
+             third = second;
+             second = array[i];
+         } else if (array[i] > third && array[i] < second) {
+             third = array[i];
+         }
+     }
+
+    console.log("The third largest integer in", array, "is", third);
+}
+
+let array = [1, 4, 53, 31, 9, 11, 43];
+thirdLargestInArray(array);
